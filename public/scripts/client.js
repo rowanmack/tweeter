2923d484f76b6d138b7fd69be54fe38e061bf9e7
Rowan Mackenzie <row.mackenzie@gmail.com>
2023-03-02 15:49:24 -0800

    responsive design styling added to nav, header, tweet sections. new profile picture.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -59,10 +59,10 @@ $(document).ready(function() {
     tweetText = $("#tweet-text").val().length
 
     if(tweetText < 1) {
-      $('#error-message').text("Your tweet is empty").slideDown('slow').delay(1000).slideUp('slow');
+      $('#error-message').text("your tweet is empty").slideDown('slow').delay(1500).slideUp('slow');
 
     } else if (tweetText > 140){
-      $('#error-message').text("Your tweet has too many characters!").slideDown('slow').delay(1000).slideUp('slow');
+      $('#error-message').text("too many characters!").slideDown('slow').delay(1500).slideUp('slow');
 
     } else {
       $.ajax({
