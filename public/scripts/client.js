233795f32dee736e4ac01eec474f6362ae7d8b1e
Rowan Mackenzie <row.mackenzie@gmail.com>
2023-03-01 16:56:13 -0800

    implement ajax get request for new tweet submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+// const tweetData = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd"
+//     },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ];
 
 const createTweetElement = function(obj) {
 
@@ -65,13 +65,24 @@ $(document).ready(function() {
 
   console.log("html loaded correctly!");
 
-  renderTweets(tweetData);
+  // renderTweets(tweetData);
 
   $( "#tweet-form" ).submit(function( event ) {
     event.preventDefault();
     $.post( "/tweets", $( this ).serialize() )
   });
 
+  function loadTweets() {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+      success: (response) => {
+        // console.log(reponse)
+        renderTweets(response)
+      }
+    })
+  };
+  loadTweets()
 });
 
 
